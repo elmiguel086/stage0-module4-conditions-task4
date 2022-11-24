38d0808af3f1de086d5dd4b5aa96569a9e81c882
elmiguel0 <miguelangel.ramos@ibm.com>
2022-11-24 10:32:52 -0600

    changes done

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int res = (first>second)?first:second;
+        res = (res>third)?res:third;
+        System.out.println(res);
     }
 }
